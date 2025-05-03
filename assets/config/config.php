aced3ea8f59add4eb47be99f8e63fc7654f33cd6
Hamzah <hamzahnafalah@gmail.com>
2025-05-03 12:50:22 +0700

    update modules

--- assets/config/config.php
+++ assets/config/config.php
@@ -1,15 +1,28 @@
 <?php
 
-use Hanafalah\ModuleSummary\Models as ModuleSummaryModels;
 use Hanafalah\ModuleSummary\Commands as ModuleSummaryCommands;
 
 return [
-    'commands' => [
-        ModuleSummaryCommands\InstallMakeCommand::class
+    'namespace' => 'Hanafalah\\ModuleSummary',
+    'app' => [
+        'contracts' => [
+            //ADD YOUR CONTRACTS HERE
+        ],
+    ],
+    'libs' => [
+        'model' => 'Models',
+        'contract' => 'Contracts',
+        'schema' => 'Schemas',
+        'database' => 'Database',
+        'data' => 'Data',
+        'resource' => 'Resources',
+        'migration' => '../assets/database/migrations'
     ],
     'database' => [
         'models' => [
-            'Summary'  => ModuleSummaryModels\Summary\Summary::class,
         ]
+    ],
+    'commands' => [
+        ModuleSummaryCommands\InstallMakeCommand::class
     ]
 ];
